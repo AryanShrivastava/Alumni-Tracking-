c774df0ab97f3614add8901362d8f5fc16c70b06
Rishabh Abhani <rishabhabhani@gmail.com>
2020-01-21 00:38:40 +0530

    ..

--- rekonnect-main/registration/views.py
+++ rekonnect-main/registration/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from . import forms
+
+class SignUp(CreateView):
+    # form_class = forms.UserSignupForm
+    # success_url = reverse_lazy('registration:login')
+    # template_name = ''
+    pass
--- rekonnect-main/rekonnect/settings.py
+++ rekonnect-main/rekonnect/settings.py
@@ -133,3 +133,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
