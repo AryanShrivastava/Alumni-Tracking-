77f2113b597bab598d13951b306e8f2ba277ef9b
Rishabh Abhani <rishabhabhani@gmail.com>
2020-01-20 11:35:56 +0530

    ..

--- rekonnect-main/rekonnect/settings.py
+++ rekonnect-main/rekonnect/settings.py
@@ -89,6 +89,13 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
