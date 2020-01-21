f1fc9b1caaae04cb199be6efb79e807a08556b43
Rishabh Abhani <rishabhabhani@gmail.com>
2020-01-21 07:24:02 +0530

    .

--- rekonnect-main/core/urls.py
+++ rekonnect-main/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, HomeView, ProfileView
+from .views import index, HomeView, ProfileView, DonationView
 from django.contrib.auth.decorators import login_required
 
 app_name = 'core'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', index.as_view(), name='index'),
     path('home/', HomeView.as_view(), name='home'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('donation/', DonationView.as_view(), name='donation'),
 ]
